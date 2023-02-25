3f071b6a0221e70f700379b292453d9deb2a35ae
undownding <undownding@gmail.com>
2023-02-26 03:14:24 +0800

    fix role service spec

--- src/user/role/role.service.spec.ts
+++ src/user/role/role.service.spec.ts
@@ -1,12 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { RoleService } from './role.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { typeOrmModuleOptions } from '@/lib/data-source'
+import { User } from '@/user/user.entity'
+import { Role } from '@/user/role/role.entity'
+import { ConfigModule } from '@nestjs/config'
+import { UserService } from '@/user/user.service'
+import { AuthService } from '@/user/auth/auth.service'
+import { AuthJwtStrategy } from '@/user/auth/auth-jwt.strategy'
+import { JwtGuard } from '@/user/auth/auth-jwt.guard'
 
 describe('RoleService', () => {
   let service: RoleService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RoleService],
+      imports: [
+        TypeOrmModule.forRoot(typeOrmModuleOptions),
+        TypeOrmModule.forFeature([User, Role]),
+        ConfigModule,
+      ],
+      providers: [
+        UserService,
+        AuthService,
+        RoleService,
+        AuthJwtStrategy,
+        JwtGuard,
+      ],
     }).compile()
 
     service = module.get<RoleService>(RoleService)
