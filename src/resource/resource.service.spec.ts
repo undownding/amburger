5e8fd56340db7a06208cb8c86b9505e2903621c5
undownding <undownding@gmail.com>
2023-03-15 11:20:29 +0800

    fix build error

--- src/resource/resource.service.spec.ts
+++ src/resource/resource.service.spec.ts
@@ -3,13 +3,19 @@ import { ResourceService } from './resource.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Resource } from '@/resource/resource.entity'
 import { Permission } from '@/resource/permission.entity'
+import { typeOrmModuleOptions } from '@/lib/data-source'
+import { User } from '@/user/user.entity'
+import { Role } from '@/user/role/role.entity'
 
 describe('ResourceService', () => {
   let service: ResourceService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [TypeOrmModule.forFeature([Resource, Permission])],
+      imports: [
+        TypeOrmModule.forRoot(typeOrmModuleOptions),
+        TypeOrmModule.forFeature([Resource, Permission, User, Role]),
+      ],
       providers: [ResourceService],
     }).compile()
 
