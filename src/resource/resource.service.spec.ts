b6ac0ed261ec6cb5e754c483a90308df9327d55c
undownding <undownding@gmail.com>
2023-03-15 11:02:13 +0800

    fix test error

--- src/resource/resource.service.spec.ts
+++ src/resource/resource.service.spec.ts
@@ -1,11 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { ResourceService } from './resource.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Resource } from '@/resource/resource.entity'
+import { Permission } from '@/resource/permission.entity'
 
 describe('ResourceService', () => {
   let service: ResourceService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TypeOrmModule.forFeature([Resource, Permission])],
       providers: [ResourceService],
     }).compile()
 
