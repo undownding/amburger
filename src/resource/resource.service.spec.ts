acdb210b91dac1fe40cb6dab7b26ba7db0d25549
undownding <undownding@gmail.com>
2023-03-20 11:16:59 +0800

    fix test

--- src/resource/resource.service.spec.ts
+++ src/resource/resource.service.spec.ts
@@ -4,8 +4,9 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { Resource } from '@/resource/resource.entity'
 import { Permission } from '@/resource/permission.entity'
 import { typeOrmModuleOptions } from '@/lib/data-source'
-import { User } from '@/user/user.entity'
-import { Role } from '@/user/role/role.entity'
+import { ConfigModule } from '@nestjs/config'
+import { UserModule } from '@/user/user.module'
+import { CacheModule } from '@nestjs/common'
 
 describe('ResourceService', () => {
   let service: ResourceService
@@ -13,8 +14,16 @@ describe('ResourceService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+        }),
+        CacheModule.register({
+          isGlobal: true,
+        }),
         TypeOrmModule.forRoot(typeOrmModuleOptions),
-        TypeOrmModule.forFeature([Resource, Permission, User, Role]),
+        TypeOrmModule.forFeature([Resource, Permission]),
+        UserModule,
+        ConfigModule,
       ],
       providers: [ResourceService],
     }).compile()
