2f5218c79ea358b06da6e25dc1f5819595b5281a
undownding <undownding@gmail.com>
2023-03-23 01:29:10 +0800

    test case for user create and password update

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -14,6 +14,7 @@ import { CacheModule } from '@nestjs/common'
 
 describe('UserService', () => {
   let service: UserService
+  let authService: AuthService
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -37,7 +38,8 @@ describe('UserService', () => {
       providers: [UserService, AuthService, RoleService, PasswordService],
     }).compile()
 
-    service = module.get<UserService>(UserService)
+    service = module.get(UserService)
+    authService = module.get(AuthService)
     await service.onModuleInit()
   })
 
@@ -49,5 +51,29 @@ describe('UserService', () => {
     const users = await service.findAll()
     expect(users).toHaveLength(1)
     expect(users[0].roles).toHaveLength(3)
+  })
+
+  it('should sign up a user', async () => {
+    const user = await service.signUp({
+      username: 'user_for_create_test',
+      password: 'old_password',
+    })
+    expect(user).toBeDefined()
+    expect(user.name).toBe('user_for_create_test')
+  })
+
+  it('should update password', async () => {
+    const user = await service.signUp({
+      username: 'user_for_update_password_test',
+      password: 'old_password',
+    })
+    const newPassword = 'new_password'
+    await service.updatePassword(user.id, newPassword)
+    const updatedUser = await service.getByUserName(
+      'user_for_update_password_test',
+      true,
+    )
+    const isCorrect = await authService.checkPassword(updatedUser, newPassword)
+    expect(isCorrect).toBeTruthy()
   })
 })
